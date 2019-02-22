befe3e75892e6e91b1d0d5aaaa79a1db904db922
Dalazo <47372362+Dalazo@users.noreply.github.com>
2019-02-21 17:06:33 -0800

    Reading txt file and copying to another txt file

---
+++ pay.cpp
@@ -0,0 +1,114 @@
+
+#include "person.h"
+#include "person.cpp"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+
+ int readData(Person arr[],int size);
+ void writeData(Person arr[],int size);
+
+int main()
+{
+int FileMax = 20;
+int fileActual;
+Person myarr[20];
+
+fileActual = readData(myarr,FileMax);
+
+
+writeData(myarr,fileActual);
+
+
+return 0;
+
+}
+
+int readData(Person arr[],int size)
+{
+
+fstream myfile;
+int counter = 0;
+string linecheck;
+Person S;
+
+
+myfile.open("data.txt");
+
+
+if (!myfile.is_open())
+{
+
+cout << "File not found " << endl;
+
+}
+
+else
+{
+
+while (getline(myfile,linecheck)){
+
+    counter++;
+}
+myfile.close();
+
+myfile.open("data.txt");
+cout << "File is open: Reading contents" << endl;
+
+while (!myfile.eof())
+{
+
+
+for (unsigned int i = 0; i < counter; i++)
+{
+string firstname;
+string lastname;
+
+float hours;
+float payrate;
+
+myfile >> firstname >> lastname >> hours >> payrate;
+
+S.setFirstName(firstname);
+S.setLastName(lastname);
+
+S.setHoursWorked(hours);
+S.setPayRate(payrate);
+
+arr[i] = S;
+
+}
+
+}
+
+}
+myfile.close();
+return counter;
+}
+
+
+
+void writeData(Person arr[],int size){
+
+
+ofstream outfile ("newData.txt");
+
+for (unsigned int i = 0; i < size; i++)
+{
+
+outfile << arr[i].fullName() << " ";
+outfile << arr[i].getHoursWorked()<< " ";
+outfile << arr[i].getPayRate() << endl;
+
+
+}
+
+
+
+outfile.close();
+
+}
---
+++ person.cpp
@@ -0,0 +1,73 @@
+
+
+#include "person.h"
+#include <string>
+
+Person::Person()
+{
+
+}
+
+void Person::setHoursWorked(float hours)
+{
+
+hoursWorked = hours;
+
+}
+    
+void Person::setFirstName(string fName)
+{
+
+firstName = fName;
+
+}    
+void Person::setLastName(string lName)
+{
+
+lastName = lName;
+
+}    
+
+void Person::setPayRate(float payrate)
+{
+
+payRate = payrate;
+
+
+}
+
+std::string Person::getFirstName()
+{
+return firstName;
+}
+
+std::string Person::getLastName()
+{
+
+return lastName;
+
+}
+
+float Person::getPayRate()
+{
+
+return payRate;
+
+}
+
+float Person::getHoursWorked()
+{
+
+return hoursWorked;
+
+}
+
+std::string Person::fullName()
+{
+
+
+return firstName + " " + lastName;
+
+}
+
+   
